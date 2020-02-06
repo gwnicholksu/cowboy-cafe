d19cfae0cf8eeb6b647d6b42b799c015bbedef38
Grant Nichol <gwnichol@ksu.edu>
2020-02-05 20:13:02 -0600

    Added ChiliCheeseFries and Uncomented Tests

---
+++ Data/ChiliCheeseFries.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A base class representing a side
+    /// </summary>
+    public class ChiliCheeseFries : Side
+    {
+        /// <summary>
+        /// Gets the price of the side
+        /// </summary>
+        public override double Price
+        { 
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 1.99;
+                    case Size.Medium:
+                        return 2.99;
+                    case Size.Large:
+                        return 3.99;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the calories of the entree
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 433;
+                    case Size.Medium:
+                        return 524;
+                    case Size.Large:
+                        return 610;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}

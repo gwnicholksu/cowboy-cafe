766191addd023ff0890e56507a9a03f2678d7ed5
Grant Nichol <gwnichol@ksu.edu>
2020-02-07 16:38:23 -0600

    Added PanDeCampo class

---
+++ Data/PanDeCampo.cs
@@ -0,0 +1,60 @@
+﻿/*
+* Author: Grant Nichol
+* Class: PanDeCampo.cs
+* Purpose: A class representing the Pan de Campo side
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Pan de Campo side
+    /// </summary>
+    public class PanDeCampo : Side
+    {
+        /// <summary>
+        /// Gets the price of the side
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 1.59;
+                    case Size.Medium:
+                        return 1.79;
+                    case Size.Large:
+                        return 1.99;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the calories of the side
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 227;
+                    case Size.Medium:
+                        return 269;
+                    case Size.Large:
+                        return 367;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}

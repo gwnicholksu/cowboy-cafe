d32de6156426298f143821b75783353bd85e9ebb
Grant Nichol <gwnichol@ksu.edu>
2020-02-07 16:26:03 -0600

    Add CornDodgers Class

---
+++ Data/CornDodgers.cs
@@ -0,0 +1,60 @@
+﻿/*
+* Author: Grant Nichol
+* Class: CornDodgers.cs
+* Purpose: A class representing the Corn Dodgers side
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Corn Dodgers side
+    /// </summary>
+    public class CornDodgers : Side
+    {
+        /// <summary>
+        /// Gets the price of the side
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 1.59;
+                    case Size.Medium:
+                        return 1.79;
+                    case Size.Large:
+                        return 1.99;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the calories of the entree
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 512;
+                    case Size.Medium:
+                        return 685;
+                    case Size.Large:
+                        return 717;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}

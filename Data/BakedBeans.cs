589cae72cdef84569b69a536adf466f9697954a0
Grant Nichol <gwnichol@ksu.edu>
2020-02-07 16:34:15 -0600

    Added BakedBeans Class

---
+++ Data/BakedBeans.cs
@@ -0,0 +1,60 @@
+﻿/*
+* Author: Grant Nichol
+* Class: CornDodgers.cs
+* Purpose: A class representing the Baked Beans side
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Baked Beans side
+    /// </summary>
+    public class BakedBeans : Side
+    {
+        /// <summary>
+        /// Gets the price of the side
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 1.59;
+                    case Size.Medium:
+                        return 1.79;
+                    case Size.Large:
+                        return 1.99;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the calories of the side
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 312;
+                    case Size.Medium:
+                        return 378;
+                    case Size.Large:
+                        return 410;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}

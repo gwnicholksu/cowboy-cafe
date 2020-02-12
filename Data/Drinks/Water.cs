967321f0ae04def606489852111723b582d9cdca
gwnicholksu <gwnichol@F1092-08-2096>
2020-02-12 14:31:18 -0600

    Add Water Class

---
+++ Data/Drinks/Water.cs
@@ -0,0 +1,60 @@
+﻿/*
+* Author: Grant Nichol
+* Class: Water.cs
+* Purpose: A class for Water
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data.Drinks
+{
+    /// <summary>
+    /// A class for Water
+    /// </summary>
+    public class Water : Drink
+    {
+        /// <summary>
+        /// The number of calories in the drink
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// The price of the drink
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                return 0.12;
+            }
+        }
+
+        /// <summary>
+        /// Whether there should be a lemon included
+        /// </summary>
+        public bool Lemon { get; set; } = false;
+
+        /// <summary>
+        /// The special instructions for this drink
+        /// </summary>
+        public override List<String> SpecialInstructions
+        {
+            get
+            {
+                var returnList = new List<String>();
+                if (!Ice) returnList.Add("Hold Ice");
+                if (Lemon) returnList.Add("Add Lemon");
+
+                return returnList;
+            }
+        }
+    }
+}

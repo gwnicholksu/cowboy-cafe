651afcfd21f4f2909e462dce795c2340b6b7591b
gwnicholksu <gwnichol@F1092-08-2096>
2020-02-12 13:39:20 -0600

    Added Jerked Soda Class

---
+++ Data/Drinks/JerkedSoda.cs
@@ -0,0 +1,70 @@
+﻿/*
+* Author: Grant Nichol
+* Class: JerkedSoda.cs
+* Purpose: A class for the Jerked Soda drink
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data.Drinks
+{
+    /// <summary>
+    /// A class for the Jerked Soda drink
+    /// </summary>
+    public class JerkedSoda : Drink
+    {
+        /// <summary>
+        /// The number of calories in the drink
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small: return 110;
+                    case Size.Medium: return 146;
+                    case Size.Large: return 198;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// The price of the drink
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small: return 1.59;
+                    case Size.Medium: return 2.10;
+                    case Size.Large: return 2.59;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        public SodaFlavor Flavor { get; set; } = SodaFlavor.CreamSoda;
+
+        /// <summary>
+        /// The special instructions for this drink
+        /// </summary>
+        public override List<String> SpecialInstructions
+        {
+            get
+            {
+                var returnList = new List<String>();
+                if (!Ice) returnList.Add("Hold Ice");
+
+                return returnList;
+            }
+        }
+    }
+}

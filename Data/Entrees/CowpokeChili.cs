41fa62a0998023ca0b6d31bbbed05713f6066f88
gwnicholksu <gwnichol@F1092-08-2096>
2020-02-12 13:03:22 -0600

    Regrouped classed for easier navigation and programing

---
+++ Data/Entrees/CowpokeChili.cs
@@ -0,0 +1,98 @@
+﻿/*
+* Author: Grant Nichol
+* Class: CowpokeChili.cs
+* Purpose: A class representing the Cowpoke Chili entree
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Cowpoke Chili entree
+    /// </summary>
+    public class CowpokeChili : Entree
+    {
+        private bool cheese = true;
+        /// <summary>
+        /// If the chili is topped with cheese
+        /// </summary>
+        public bool Cheese
+        {
+            get { return cheese; }
+            set { cheese = value; }
+        }
+
+        private bool sourCream = true;
+        /// <summary>
+        /// If the chili is topped with sour cream
+        /// </summary>
+        public bool SourCream
+        {
+            get { return sourCream; }
+            set { sourCream = value; }
+        }
+
+        private bool greenOnions = true;
+        /// <summary>
+        /// If the chili is topped with green onions
+        /// </summary>
+        public bool GreenOnions
+        {
+            get { return greenOnions; }
+            set { greenOnions = value; }
+        }
+
+        private bool tortillaStrips = true;
+        /// <summary>
+        /// If the chili is topped with tortilla strips
+        /// </summary>
+        public bool TortillaStrips
+        {
+            get { return tortillaStrips; }
+            set { tortillaStrips = value; }
+        }
+
+        /// <summary>
+        /// The price of the chili
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                return 6.10;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the chili
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                return 171;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the chili
+        /// </summary>
+        public override List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!cheese) instructions.Add("hold cheese");
+                if (!sourCream) instructions.Add("hold sour cream");
+                if (!greenOnions) instructions.Add("hold green onions");
+                if (!tortillaStrips) instructions.Add("hold tortilla strips");
+
+                return instructions;
+            }
+        }
+    }
+}
+
---
+++ Data/Entrees/DakotaDoubleBurger.cs
@@ -0,0 +1,142 @@
+﻿/*
+* Author: Grant Nichol
+* Class: DakotaDoubleBurger.cs
+* Purpose: A class representing the Dakota Double Burger entree
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Dakota Double Burger entree
+    /// </summary>
+    public class DakotaDoubleBurger : Entree
+    {
+        /// <summary>
+        /// The price of the burger
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                return 5.20;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the burger
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                return 464;
+            }
+        }
+
+        private bool ketchup = true;
+        /// <summary>
+        /// Wether or not to have ketchup
+        /// </summary>
+        public bool Ketchup
+        {
+            get { return ketchup; }
+            set { ketchup = value; }
+        }
+
+        private bool mustard = true;
+        /// <summary>
+        /// Wether or not to have mustard
+        /// </summary>
+        public bool Mustard
+        {
+            get { return mustard; }
+            set { mustard = value; }
+        }
+
+        private bool pickle = true;
+        /// <summary>
+        /// Wether or not to have pickle
+        /// </summary>
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        private bool cheese = true;
+        /// <summary>
+        /// Wether or not to have cheese
+        /// </summary>
+        public bool Cheese
+        {
+            get { return cheese; }
+            set { cheese = value; }
+        }
+
+        private bool tomato = true;
+        /// <summary>
+        /// Wether or not to have tomato
+        /// </summary>
+        public bool Tomato
+        {
+            get { return tomato; }
+            set { tomato = value; }
+        }
+
+        private bool lettuce = true;
+        /// <summary>
+        /// Wether or not to have lettuce
+        /// </summary>
+        public bool Lettuce
+        {
+            get { return lettuce; }
+            set { lettuce = value; }
+        }
+
+        private bool mayo = true;
+        /// <summary>
+        /// Wether or not to have mayo
+        /// </summary>
+        public bool Mayo
+        {
+            get { return mayo; }
+            set { mayo = value; }
+        }
+
+        private bool bun = true;
+        /// <summary>
+        /// Wether or not to have a bun
+        /// </summary>
+        public bool Bun
+        {
+            get { return bun; }
+            set { bun = value; }
+        }
+
+        /// <summary>
+        /// Get the list of special instruction
+        /// </summary>
+        public override List<String> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<String>();
+
+                if (!ketchup) instructions.Add("hold ketchup");
+                if (!pickle) instructions.Add("hold pickle");
+                if (!mustard) instructions.Add("hold mustard");
+                if (!cheese) instructions.Add("hold cheese");
+                if (!tomato) instructions.Add("hold tomato");
+                if (!lettuce) instructions.Add("hold lettuce");
+                if (!mayo) instructions.Add("hold mayo");
+                if (!bun) instructions.Add("hold bun");
+
+                return instructions;
+            }
+        }
+    }
+}

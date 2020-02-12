c21d4d39df30fa412819ea7ddda3707fe6269174
gwnicholksu <gwnichol@F1092-08-2096>
2020-02-12 13:12:40 -0600

    Add Abstract Drink Class

---
+++ Data/Drink.cs
@@ -0,0 +1,40 @@
+﻿/*
+* Author: Grant Nichol
+* Class: Drink.cs
+* Purpose: An abstract class detailing what should be in an drink class.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public abstract class Drink
+    {
+        /// <summary>
+        /// The number of calories in the drink
+        /// </summary>
+        public abstract uint Calories { get; }
+
+        /// <summary>
+        /// The price of the drink
+        /// </summary>
+        public abstract double Price { get; }
+
+        /// <summary>
+        /// The size of the drink
+        /// </summary>
+        public Size Size { get; set; } = Size.Small;
+
+        /// <summary>
+        /// Whether there should be ice in the drink
+        /// </summary>
+        public bool Ice { get; set; } = true;
+
+        /// <summary>
+        /// The special instructions for this drink
+        /// </summary>
+        public abstract List<String> SpecialInstructions { get; }
+    }
+}

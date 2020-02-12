f99a0b3d5d87c7a8127c69d27d1f91d624f2270f
gwnicholksu <gwnichol@F1092-08-2096>
2020-02-12 13:18:30 -0600

    Add SodaFlavor enum

---
+++ Data/Drinks/SodaFlavor.cs
@@ -0,0 +1,24 @@
+﻿/*
+* Author: Grant Nichol
+* Class: SodaFlavor.cs
+* Purpose: An enum of what soda flavors are available
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data.Drinks
+{
+    /// <summary>
+    /// An enum of what soda flavors are available
+    /// </summary>
+    public enum SodaFlavor
+    {
+        CreamSoda,
+        OrangeSoda,
+        Sarsaparilla,
+        BirchBeer,
+        RootBeer
+    }
+}

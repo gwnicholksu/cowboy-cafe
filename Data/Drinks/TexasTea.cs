a0fa575ece6ff8b3a3855d1c1ea3036fa6e63844
gwnicholksu <gwnichol@F1092-08-2096>
2020-02-12 14:25:35 -0600

    Add TexasTea Class

---
+++ Data/Drinks/TexasTea.cs
@@ -0,0 +1,73 @@
+﻿/*
+* Author: Grant Nichol
+* Class: TexasTea.cs
+* Purpose: A class for the Texas Tea drink
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data.Drinks
+{
+    /// <summary>
+    /// A class for the Texas Tea drink
+    /// </summary>
+    public class TexasTea : Drink
+    {
+        /// <summary>
+        /// The number of calories in the drink
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                uint cal = Size switch
+                {
+                    Size.Small => 10,
+                    Size.Medium => 22,
+                    Size.Large => 36,
+                    _ => throw new NotImplementedException(),
+                };
+
+                return (Sweet) ? cal / 2 : cal;
+            }
+        }
+
+        /// <summary>
+        /// The price of the drink
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small: return 1.00;
+                    case Size.Medium: return 1.50;
+                    case Size.Large: return 2.00;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        public bool Sweet { get; set; } = true;
+        public bool Lemon { get; set; } = false;
+
+        /// <summary>
+        /// The special instructions for this drink
+        /// </summary>
+        public override List<String> SpecialInstructions
+        {
+            get
+            {
+                var returnList = new List<String>();
+                if (!Ice) returnList.Add("Hold Ice");
+                if (Lemon) returnList.Add("Add Lemon");
+
+                return returnList;
+            }
+        }
+    }
+}
